63ead88792e12bf382fedc5aa1a85624e4352790
josue moises perez reyes <moisesperez0895@gmail.com>
2023-03-09 10:05:02 -0600

    mi aporte

--- calculadora.py
+++ calculadora.py
@@ -7,9 +7,16 @@ def suma(numero1, numero2):
 
 def resta(numero1, numero2):
     total = numero1 - numero2
+    return total 
+
+def multiplicacion(numero1, numero2):
+    total = numero1 * numero2
     return total
 
 resultado = suma(num1, num2)
 resultado2 = resta(num1, num2)
+resultado3 = multiplicacion(num1, num2)
 
-print('El resultado es: ',resultado2)
+print('El resultado es: ',resultado)
+print('El resultado de la resta es: ', resultado2)
+print('El resultado de la multiplicación es: ', resultado3)
